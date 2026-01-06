6e43f8b863a8afea89dfcb984dea4e7101279b82
Yanick Schraner <yanick.schraner@bs.ch>
2026-01-06 13:58:57 +0100

    Refactor README to remove outdated DSPy utilities section and add release instructions; introduce GitHub Actions workflow for automated PyPI publishing.

--- src/backend_common/dspy_common/__init__.py
+++
@@ -1,6 +0,0 @@
-from .adapters import DisableReasoningAdapter
-from .listeners import SwissGermanStreamListener
-from .metrics import edit_distance_metric
-from .module import AbstractDspyModule
-
-__all__ = ["AbstractDspyModule", "DisableReasoningAdapter", "SwissGermanStreamListener", "edit_distance_metric"]
